0db8a3c31584102f5cba1a1e88f6b815714887d0
highshot-rk <ivannosal22@gmail.com>
2021-06-25 14:18:51 +0900

    add eslint and init

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("server")
+const express = require("express")
+const bodyParser = require("body-parser")
+
+var server = express();
+
+server.listen(3000, () => {
+    console.log("server is working")
+});
