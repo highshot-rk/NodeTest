0d849125b28950cd41a2cdadba7d906be3e1afeb
highshot-rk <ivannosal22@gmail.com>
2021-06-25 16:07:56 +0900

    3 step complete

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios")
+const URL = "https://hummingbird-staging.podgroup.com"
+const NetError = {
+    result: false,
+    status: "---",
+    statusCode: "Unknown wrror, may be internet connection error",
+}
 
 var server = express()
 
@@ -23,10 +29,10 @@ server.post("/auth", (req, res) => {
     if (username === "" || password === "") {
         res.json({
             result: false,
-            message: "fill the username and password"
+            statusMessage: "fill the username and password"
         })
     } else {
-        axios.post("https://hummingbird-staging.podgroup.com/auth/token", {
+        axios.post(URL + "/auth/token", {
             username: username,
             password: password,
         }).then(swaggerResp => {
@@ -43,15 +49,64 @@ server.post("/auth", (req, res) => {
                     statusMessage: err.response.data.message,
                 })
             } else {
-                res.json({
-                    result: false,
-                    status: "---",
-                    statusCode: "Unknown wrror, may be internet connection error",
-                })
+                res.json(NetError)
             }
         })
     }
 })
+
+// Create a method that allow to create users in the account
+// users (token, username, password...)
+server.post("/users", (req, res) => {
+    var token = req.headers["x-access-token"] ? req.headers["x-access-token"] : "";
+    var username = req.body.username ? req.body.username : "";
+    var password = req.body.password ? req.body.password : "";
+    var email = req.body.email ? req.body.email : "";
+    var accountId = req.body.accountId ? req.body.accountId : "";
+    var status = req.body.status ? req.body.status : "";
+    var permissions = req.body.permissions ? req.body.permissions : "";
+    try {
+        permissions = JSON.parse(permissions)
+    } catch (error) {
+        permissions = []
+    }
+    if (username === "" || password === "" || email == "" || accountId == "" || status == "") {
+        res.json({
+            result: false,
+            statusMessage: "fill the parameters"
+        })
+    } else {
+        axios.post(URL+ "/users", {
+            accountId: accountId,
+            username: username,
+            password: password,
+            email: email,
+            status: status,
+            permissions: permissions,
+        }, {
+            headers: {
+                "x-access-token": token
+            }
+        }).then(res1 => {
+            res.json({
+                result: true,
+                ...res1.data,
+            })
+        }).catch(err => {
+            if (err.response) {
+                res.json({
+                    result: false,
+                    status: err.response.status,
+                    statusCode: err.response.data.code,
+                    statusMessage: err.response.data.message,
+                });
+            } else {
+                res.json(NetError)
+            }
+        })
+    }
+});
+
 
 // run server
 server.listen(3000, () => {
--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 var app = require('../server');
 var chai = require('chai');
+var faker = require('faker');
 var request = require('supertest');
 
 var expect = chai.expect;
@@ -21,6 +22,31 @@ describe('RestAPI tests', function() {
         request(app)
         .post('/auth')
         .send({username: "sagar.neupane", password: "enapuen.ragas"})
+        .expect(200)
+        .end(function(err, res) {
+            expect(res.body.result).equal(true)
+            done();
+        });
+    });
+
+    // test create user in account
+    it('Create user testing...', function(done) {
+        request(app)
+        .post('/users')
+        .set({ "x-access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2MGQ0NWRmZGFjZGJlYjAwMjA0OGMwMTMifQ.6Z2d-GtiKUo4EeuDbsS5_97dbAG09jVKxW_55QDcTOA" })
+        .send({
+            username: faker.internet.userName(),
+            password: "enapuen.ragas",
+            email: "sagar.neupane@testemail.com",
+            accountId: "65e5f451-54e3-45ea-84e9-dcdd8c59078e",
+            status: "active",
+            permissions: [{
+                "accountId": "65e5f451-54e3-45ea-84e9-dcdd8c59078e",
+                "roles": [
+                    "admin"
+                ],
+            }]
+        })
         .expect(200)
         .end(function(err, res) {
             expect(res.body.result).equal(true)
